b4992513611f9f6ef696cf8ad3232c5bd03dc6fc
camilagarcia36 <garciiaperezz@gmail.com>
2023-04-04 11:18:56 +0200

    proyecto inicial

---
+++ ahorcado/src/main/java/ahorcado_juego/ahorcado/App.java
@@ -0,0 +1,13 @@
+package ahorcado_juego.ahorcado;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ahorcado/src/test/java/ahorcado_juego/ahorcado/AppTest.java
@@ -0,0 +1,20 @@
+package ahorcado_juego.ahorcado;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
