b082c41e81dcc4a118df1df082fd7466c5fa70f2
Admin <garciiaperezz@gmail.com>
2023-04-10 21:03:08 +0200

    cambios nuevos

--- ahorcado/src/main/java/ahorcado_main/App.java
+++ ahorcado/src/main/java/ahorcado_main/App.java
@@ -1,13 +1,46 @@
 package ahorcado_main;
 
+import java.awt.EventQueue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Hashtable;
+
+
+import view.FormularioInicio;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static FormularioInicio form;
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	
+    	//Creamos un hashtable con las palabras a utilizar.  
+    	Hashtable<String, ArrayList<String>> palabrasDoc = new Hashtable<String, ArrayList<String>>();
+		palabrasDoc.put("Superhéroes", new ArrayList<String>(Arrays.asList("spiderman", "hulk", "groot")));
+
+		palabrasDoc.put("Deportes", new ArrayList<String>(Arrays.asList("tenis", "baloncesto", "balomano")));
+		
+		
+		
+		//
+    
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					form = new FormularioInicio();
+
+			
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
     }
 }
--- ahorcado/src/main/java/view/IntrerfazJuego.java
+++ ahorcado/src/main/java/view/IntrerfazJuego.java
@@ -10,25 +10,7 @@ public class IntrerfazJuego extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					IntrerfazJuego frame = new IntrerfazJuego();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
 
-	/**
-	 * Create the frame.
-	 */
 	public IntrerfazJuego() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
